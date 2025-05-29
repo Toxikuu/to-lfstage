d5646309393b00818e9d45e04178fc8e1a735ea6
Toxikuu <toxikuu@users.noreply.github.com>
2025-05-29 18:28:41 -0500

    use an ncurses snapshot

--- scripts/15-stage2.sh
+++ scripts/15-stage2.sh
@@ -27,9 +27,11 @@ make DESTDIR="$LFS" install
 
 
 # Ncurses
+# NOTE: A specific ncurses snapshot is used (ncurses-6.5-20250517) to avoid
+# gcc >= 15 errors
 pre ncurses
 
-mkdir build
+mkdir -v build
 pushd build
     ../configure AWK=gawk
     make -C include
